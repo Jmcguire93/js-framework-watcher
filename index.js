568f22e02f0b3cb20ee54362e3681cb05bab6399
James <james.ryan.mcguire@gmail.com>
2021-10-14 17:04:51 -0400

    Builds chart appropriately on page load

--- index.js
+++ index.js
@@ -4,61 +4,58 @@ let stars = [];
 let watchers = [];
 let forks = [];
 
-axios
-  .get("https://api.github.com/repos/vuejs/vue")
-  .then(function (response) {
-    // handle success
-    console.log(response.data);
-    stars.push(response.data.stargazers_count);
-    watchers.push(response.data.watchers_count);
-    forks.push(response.data.forks_count);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+axios.get("https://api.github.com/repos/vuejs/vue").then(function (response) {
+  // handle success
+  console.log(response.data);
+  stars.push(response.data.stargazers_count);
+  watchers.push(response.data.watchers_count);
+  forks.push(response.data.forks_count);
+  console.log(stars, "Stars");
+  console.log(watchers, "Watchers");
+  console.log(forks, "Forks");
 
-console.log(stars, "Stars");
-console.log(watchers, "Watchers");
-console.log(forks, "Forks");
-
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Stars", "Watchers", "Forks"],
-    datasets: [
-      {
-        label: "# Stars, Watchers and Forks",
-        data: [stars, watchers, forks],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Stars", "Watchers", "Forks"],
+      datasets: [
+        {
+          label: "# Stars, Watchers and Forks",
+          data: [stars, watchers, forks],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
       },
     },
-  },
+  });
 });
+// .catch(function (error) {
+//   // handle error
+//   console.log(error);
+// })
+// .then(function () {
+//   // always executed
+// });
